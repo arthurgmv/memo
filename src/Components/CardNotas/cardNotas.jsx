45077f85dc773c4aaa366b1f8db578af84cb153e
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-08-09 11:48:34 -0300

    small fixes

--- src/Components/CardNotas/cardNotas.jsx
+++ src/Components/CardNotas/cardNotas.jsx
@@ -5,7 +5,7 @@ export default class CardNota extends React.Component {
         return (
             <section className="card-nota">
                 <header className="card-nota_cabecalho">
-                    <h3 className="card-nota_titulo">Título</h3>
+                    <h3 className="card-nota_titulo" style={{fontWeight: "bold"}}>Título</h3>
                 </header>
                 <p className="card-nota_texto">Escreva sua nota</p>
             </section>
